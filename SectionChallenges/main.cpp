f9466f16fe701d8d5ae433eb64e6a4d98e09bb9b
Bodike-S <lilishka7171@gmail.com>
2022-06-16 11:02:55 +0300

    Section 4 Getting Started

--- SectionChallenges/main.cpp
+++ SectionChallenges/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main() {
 
+	int favorite;
+
+	cout << "Enter your favorite number between 1 and 100: " << endl;
+	cin >> favorite;
+	if (1 <= favorite && favorite <= 100)
+		cout << "That's my favorite number too.\nNo really " << favorite << " is my favorite number.";
+	else
+		cout << "That's number more 100 or less 1";
 
 	return 0;
 }
