48c9e28dadcd9f51e7a14d0f0130dad4cad6e4d3
Bodike-S <lilishka7171@gmail.com>
2022-06-19 00:53:08 +0300

    Section 8 Statements and Operators

--- SectionChallenges/main.cpp
+++ SectionChallenges/main.cpp
@@ -6,14 +6,56 @@ using namespace std;
 void Section4();
 void Section6();
 void Section7();
+void Section8();
+
 
 int main() {
-	Section4();
-	Section6();
-	Section7();
-
+	//Section4();
+	//Section6();
+	//Section7();
+	Section8();
 	return 0;
 }
+
+void Section8()
+{
+	const int dollar{ 100 };
+	const int quarter{ 25 };
+	const int dime{ 10 };
+	const int nickel{ 5 };
+	
+	int amount;
+	int dollars, quarters, dimes, nickels, pennies; //quarters - 25, dimes - 10, nickels - 5, pennies - 1
+	cout << "Enter an amount in cents: ";
+	cin >> amount;
+	cout << "You can provide change for this change as follows:" << endl;
+	if (amount >= 0)
+	{
+		if (amount >= dollar) {
+			dollars = amount / dollar;
+			amount -= dollars * dollar;
+			cout << "dollars: " << dollars<< endl;
+		}
+		if (amount < dollar) {
+			quarters = amount / quarter;
+			amount -= quarters * quarter;
+			cout << "quarters: " << quarters << endl;
+		}
+		if (amount < quarter) {
+			dimes = amount / dime;
+			amount -= dimes * dime;
+			cout << "dimes: " << dimes << endl;
+		}
+		if (amount < dime) {
+			nickels = amount / nickel;
+			amount -= nickels * nickel;
+			cout << "nickels: " << nickels << endl;
+		}
+		pennies = amount;
+		cout << "pennies: " << pennies << endl;
+	}
+}
+
 void Section4() {
 	//Section 4
 	int favorite;
