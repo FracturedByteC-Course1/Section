5fd733cd8b8e4624cf0556a3de54a74131ebacf2
Bodike-S <lilishka7171@gmail.com>
2022-06-24 00:32:02 +0300

    Section 18 Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Account.cpp
@@ -0,0 +1,46 @@
+#include "Account.h"
+
+ostream& operator<<(ostream& os, const Account& account)
+{
+    os << "[Account: " << account.name << ": " << account.balance << ']';
+    return os;
+}
+
+Account::Account(string _name, double _balance) : name(_name), balance(_balance) 
+{
+    if (balance < 0.0)
+        throw IllegalBalanceException();
+}
+
+bool Account::deposit(double amount)
+{
+    if(amount < 0)
+        return false;
+    else {
+        balance += amount;
+        return true;
+    }
+}
+
+bool Account::withdraw(double amount)
+{
+    if (balance - amount >= 0)
+    {
+        balance -= amount;
+        return true;
+    }
+    else
+        return false;
+}
+
+void Account::print(ostream& os) const
+{
+    os.precision(2);
+    os << fixed;
+    os << "[Account: " << name << ": " << balance << ']';
+}
+
+//double Account::get_balance() const
+//{
+//    return balance;
+//}
--- ExceptionHandling/ExceptionHandling/Account.h
+++ ExceptionHandling/ExceptionHandling/Account.h
@@ -2,10 +2,11 @@
 #include<iostream>
 #include<string>
 #include"I_Printable.h"
+#include"IllegalBalanceException.h"
 
 using namespace std;
 
-class Account
+class Account:public I_Printable
 {
 	friend ostream& operator<<(ostream& os, const Account& account);
 private:
@@ -16,10 +17,10 @@ protected:
 	double balance;
 public:
 	Account(string _name = def_name, double _balance = def_balance);
-	bool deposit(double amount);
-	bool withdraw(double amount);
-	void print(ostream& os) const;
-	~Account() {};
+	virtual bool deposit(double amount) = 0;
+	virtual bool withdraw(double amount) = 0;
+	virtual void print(ostream& os) const override;
+	~Account() = default;
 	//double get_balance() const;
 };
 
